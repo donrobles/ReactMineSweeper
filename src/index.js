3e50cc4a2b7275866e0216221fcf77d9d1dc0847
Don Robles <tinkerthinker@outlook.com>
2017-02-12 00:33:02 -0800

    Got most of the code laid out for rendering the 5x5 board, but am having trouble doing it. Removed the calculateWinner function as it was no longer needed.

--- src/index.js
+++ src/index.js
@@ -16,42 +16,43 @@ function Square(props) {
   );
 }
 
-class Board extends React.Component {
-
+class Row extends React.Component {
   /*Return the React Stateless Functional Component called 'Square'.
    When returned, pass Square it's matching value in the 'squares' array of 'props' of Board.
    Also, pass it a function to update the 'squares' array in the 'state' of Game.*/
-  renderSquare(i) {
-    const squares = this.props.squares;
-    return <Square value={squares[i]} onClick={() => this.props.onClick(i)}/>;
-  }
-
-  renderRow(row) {
-    debugger;
-    const field = this.props.field;
-    const currentRow = field[row];
-    for (let i = 0; i < currentRow.length; i++) {
-      this.renderSquare(i);
-    }
+  renderSquare(value) {
+    return <Square value={value} onClick={() => this.props.onClick()}/>;
   }
 
   render() {
+    let row = this.props.row;
     return (
-      <div>
+      this.renderSquare("x")
+    )
+  }
+}
+
+class Board extends React.Component {
+
+  render() {
+    debugger;
+    let field = this.props.field;
+    return (
+      <div className="board">
         <div className="board-row">
-          {(this.renderRow(0))}
+          <Row row={field[0]} onClick={this.props.onClick()}/>
         </div>
         <div className="board-row">
-          {(this.renderRow(1))}
+          <Row row={field[1]} onClick={this.props.onClick()}/>
         </div>
         <div className="board-row">
-          {(this.renderRow(2))}
+          <Row row={field[2]} onClick={this.props.onClick()}/>
         </div>
         <div className="board-row">
-          {(this.renderRow(3))}
+          <Row row={field[3]} onClick={this.props.onClick()}/>
         </div>
         <div className="board-row">
-          {(this.renderRow(4))}
+          <Row row={field[4]} onClick={this.props.onClick()}/>
         </div>
       </div>
     );
@@ -71,17 +72,12 @@ class Game extends React.Component {
       Array(5).fill(null)
     ];
     let mineCoordinates = this.mineCoords(), xyCoordinates;
-    debugger;
-    if (fullField[0] === fullField[4]) {
-      console.log("True");
-    }
     for (let j = 0; j < mineCoordinates.length; j++) {
       xyCoordinates = mineCoordinates[j].split(",");
       let x = xyCoordinates[0];
       let y = xyCoordinates[1];
       fullField[x][y] = "X";
     }
-
     this.state = {
       field: fullField,
     };
@@ -130,45 +126,3 @@ ReactDOM.render(
   <Game />,
   document.getElementById('container')
 );
-
-function calculateWinner(squares) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-  for (let i = 0; i < lines.length; i++) {
-    //Create an array with entries a, b, and c set to one winning line from the list of potential wins.
-    const [a, b, c] = lines[i];
-    /*
-     What's being done here is straightforward once you sit and think about it.
-
-     First, lets convert the array values like so to make it easier to read...
-     A = squares[a]
-     B = squares[b]
-     C = squares[c]
-
-     We can say that if...
-     A != null
-     Then continue checking if...
-     A === B
-     And if so, does...
-     A === C
-
-     The reason for this is because logically if...
-     A = B, and A = C, then you can assume B = C because another way to write it would be...
-     B = A = C
-
-     If any of these checks fail, there's no potential for a winning line to exist.
-     */
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
-    }
-  }
-  return null;
-}
