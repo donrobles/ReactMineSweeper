476b0599eda9fac58183460781ce3b9ceae4c468
Don Robles <tinkerthinker@outlook.com>
2017-02-13 20:01:31 -0800

    Moved game code out of index.js to game.js, and created menu.js.

---
+++ src/menu.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+/**
+ * Created by pc on 2/13/2017.
+ */
+export class Menu extends React.Component {
+  render() {
+    return (
+      <div className="menu">
+        <h2>Menu</h2>
+      </div>
+    )
+  }
+}
