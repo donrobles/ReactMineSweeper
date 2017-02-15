01c922fa041b9efffd87e5efd84dfede231efb5c
Don Robles <tinkerthinker@outlook.com>
2017-02-15 13:45:45 -0800

    Refactored the code to change the format that mine coordinates are returned in to reduced the number of split() operations being done. Also added comments to each of the functions and in-line comments to explain various portions of the code.

--- src/game.js
+++ src/game.js
@@ -53,14 +53,26 @@ export class Game extends React.Component {
     };
   }
 
+  /**
+   * Generate a square mine field of size 'fieldSize' with 'numberOfMines' mines.
+   * @param fieldSize The size of the field, squared, to be generated.
+   * @param numberOfMines The number of mines the field should have.
+   * @returns {Array.<*>} The mine field of the given size, containing the given number of mines.
+   */
   generateMineField(fieldSize, numberOfMines) {
     let fullField = this.generateField(fieldSize); //Create an empty field.
-    let mineCoordinates = this.getMineCoords(numberOfMines); //Get random mine locations.
+    let mineCoordinates = this.getMineCoords(fieldSize, numberOfMines); //Get random mine locations.
     fullField = this.insertMines(fullField, mineCoordinates); //Insert mines into the field.
     fullField = this.addMineIndicators(fullField, mineCoordinates); //Add mine indicators to field.
     return fullField;
   }
 
+  /**
+   * Generate a square field with 'fieldSize' rows and columns.
+   * IE. A fieldSize of 6 will return a field with 6 rows and 6 columns for 36 squares.
+   * @param fieldSize The value to use for the square field.
+   * @returns {Array.<*>} A 2-dimensional array with 'fieldSize' rows and columns.
+   */
   generateField(fieldSize) {
     let baseArr = new Array(fieldSize).fill(null);
     baseArr.forEach(function (row, index, array) {
@@ -69,41 +81,59 @@ export class Game extends React.Component {
     return baseArr;
   }
 
-  /*
-   Pick the coordinates for the number of mines given.
+  /**
+   * Generate random coordinates for 'mineCount' number of mines in a 'fieldSize' field.
+   * @param fieldSize The size of the field to generate the mine coordinatews within.
+   * @param mineCount The number of mine coordinates to create.
+   * @returns {*|Array.<T>} A 2-dimensional array with the X and Y values of the mines at MineArray[mineNumber][0=X or 1=Y]
    */
-  getMineCoords(numOfMines) {
-    let randPicks = Array(numOfMines).fill(null); //Create empty array the size of the number of mines.
+  getMineCoords(fieldSize, mineCount) {
+    let randPicks = Array(mineCount).fill(null); //Create empty array the size of the number of mines.
     let picked = 0;
-    let xy = "";
     while (picked < randPicks.length) {
-      xy = (Math.floor(Math.random() * 5)) + "," + (Math.floor(Math.random() * 5));
+      let xy = (Math.floor(Math.random() * fieldSize)) + "," + (Math.floor(Math.random() * fieldSize));
       if (!randPicks.includes(xy)) { //Make sure the random number hasn't already been picked.
         randPicks[picked] = xy; //Add random number to array of random numbers picked.
         picked++;
       }
     }
+    //Now convert the string values in the array to integers.
+    randPicks.forEach(function (entry, index, array) {
+      let xyArr = entry.split(",");
+      array[index] = [parseInt(xyArr[0]), parseInt(xyArr[1])];
+    });
     return randPicks;
   }
 
+  /**
+   * Take the given 'fullField' and insert mines at the given 'mineCoordinates'.
+   * @param fullField The field to insert mines into.
+   * @param mineCoordinates The coordinates of the mines to insert.
+   * @returns {*} The same 'fullField' given with mines inserted.
+   */
   insertMines(fullField, mineCoordinates) {
     mineCoordinates.forEach(function (entry, index, array) {
-      let xyCoordinates = entry.split(",");
-      fullField[xyCoordinates[0]][xyCoordinates[1]] = "X";
+      fullField[entry[0]][entry[1]] = "X";
     });
     return fullField;
   }
 
+  /**
+   * Add the mine indicators to the 'fullField' around each mine at 'mineCoordinates'.
+   * @param fullField The field to add the mine indicators to.
+   * @param mineCoordinates The coordinates of the mines around which indicators should be added.
+   * @returns {*} The same 'fullField' given with mine indicators added.
+   */
   addMineIndicators(fullField, mineCoordinates) {
-    for (let i = 0; i < mineCoordinates.length; i++) {
+    mineCoordinates.forEach(function (coordinates, index, array) {
+      //There will only ever be 8 squares around each mine, so create a static array.
       let mineIndicators = [Array(2), Array(2), Array(2), Array(2), Array(2), Array(2), Array(2), Array(2)];
-      //Grab the first set of coordinates.
-      let xyCoordinates = mineCoordinates[i].split(",");
-      let x = parseInt(xyCoordinates[0]);
-      let y = parseInt(xyCoordinates[1]);
+      //Grab the X and Y coordinates.
+      let x = coordinates[0];
+      let y = coordinates[1];
       let rowColEnd = fullField.length - 1;
 
-      //Do the arithmetic for each of the 8 squares around a mine.
+      //Do the arithmetic for each of the 8 locations around a mine.
       mineIndicators[0] = [x - 1, y - 1];
       mineIndicators[1] = [x - 1, y];
       mineIndicators[2] = [x - 1, y + 1];
@@ -129,11 +159,12 @@ export class Game extends React.Component {
         });
       }
 
+      //Iterate through indicator locations, incrementing only for valid locations and where there's no mine.
       mineIndicators.forEach(function (entry, index, array) {
-        if (entry !== null) {
+        if (entry !== null) { //Check if the location is within the field.
           let fieldValue = fullField[entry[0]][entry[1]];
-          if (fieldValue !== "X") {
-            if (fieldValue === null) {
+          if (fieldValue !== "X") { //Check that the location isn't a mine.
+            if (fieldValue === null) { //Check if the location has a value already.
               fullField[entry[0]][entry[1]] = 1;
             } else {
               fullField[entry[0]][entry[1]]++;
@@ -141,12 +172,13 @@ export class Game extends React.Component {
           }
         }
       });
-    }
+    });
     return fullField;
   }
 
   handleClick(i) {
-
+    //Will get around to this eventually.
+    //Will handle logic of clicking different squares, if it's clear, a mine, number, etc.
   }
 
   render() {
