77de0317524d745b77d3b042b044dde465a0b0e1
Don Robles <tinkerthinker@outlook.com>
2017-02-16 15:22:33 -0800

    Refactored the code so that fields are generated with separate X and Y values.

--- src/game.js
+++ src/game.js
@@ -18,19 +18,14 @@ function Square(props) {
 }
 
 function Row(props) {
-
-  function generateColumns(props) {
-    let rowLength = props.row.length;
-    let columns = new Array(rowLength);
-    for (let i = 0; i < rowLength; i++) {
-      columns[i] = <Square key={"col-" + i} id={"col-" + i} value={props.row[i]} onClick={() => props.onClick()}/>
-    }
-    return columns;
+  let rowLength = props.row.length;
+  let columns = new Array(rowLength);
+  for (let i = 0; i < rowLength; i++) {
+    columns[i] = <Square key={"col-" + i} id={"col-" + i} value={props.row[i]} onClick={() => props.onClick()}/>
   }
-
   return (
     <div className="board-row">
-      {generateColumns(props)}
+      {columns}
     </div>
   )
 }
@@ -57,7 +52,7 @@ class Board extends React.Component {
 export class Game extends React.Component {
   constructor() {
     super();
-    let fullField = this.generateMineField(20, 50);
+    let fullField = this.generateMineField(20, 20, 50);
     this.state = {
       field: fullField,
     };
@@ -69,38 +64,39 @@ export class Game extends React.Component {
    * @param numberOfMines The number of mines the field should have.
    * @returns {Array.<*>} The mine field of the given size, containing the given number of mines.
    */
-  generateMineField(fieldSize, numberOfMines) {
-    let fullField = generateField(fieldSize); //Create an empty field.
-    let mineCoordinates = getMineCoords(fieldSize, numberOfMines); //Get random mine locations.
+  generateMineField(fieldSizeX, fieldSizeY, numberOfMines) {
+    let fullField = generateField(fieldSizeX, fieldSizeY); //Create an empty field.
+    let mineCoordinates = getMineCoords(fullField, numberOfMines); //Get random mine locations.
     fullField = insertMines(fullField, mineCoordinates); //Insert mines into the field.
     fullField = addMineIndicators(fullField, mineCoordinates); //Add mine indicators to field.
     return fullField;
 
     /**
-     * Generate a square field with 'fieldSize' rows and columns.
-     * IE. A fieldSize of 6 will return a field with 6 rows and 6 columns for 36 squares.
-     * @param fieldSize The value to use for the square field.
-     * @returns {Array.<*>} A 2-dimensional array with 'fieldSize' rows and columns.
+     * Generate a field with 'fieldX' rows and 'fieldY' columns.
+     * @param fieldX The height of the field to be created.
+     * @param fieldY The width of the field to be created.
+     * @returns {Array.<*>} A 2-dimensional array with 'fieldX' rows and 'fieldY' columns.
      */
-    function generateField(fieldSize) {
-      let baseArr = new Array(fieldSize).fill(null);
+    function generateField(fieldX, fieldY) {
+      let baseArr = new Array(fieldX).fill(null);
       baseArr.forEach(function (row, index, array) {
-        array[index] = new Array(fieldSize).fill(null);
+        array[index] = new Array(fieldY).fill(null);
       });
       return baseArr;
     }
 
     /**
-     * Generate random coordinates for 'mineCount' number of mines in a 'fieldSize' field.
-     * @param fieldSize The size of the field to generate the mine coordinatews within.
+     * Generate random coordinates for 'mineCount' number of mines in the given 'fullField'.
+     * @param fullField The field to generate the mine coordinates within.
      * @param mineCount The number of mine coordinates to create.
      * @returns {*|Array.<T>} A 2-dimensional array with the X and Y values of the mines at MineArray[mineNumber][0=X or 1=Y]
      */
-    function getMineCoords(fieldSize, mineCount) {
+    function getMineCoords(fullField, mineCount) {
       let randPicks = Array(mineCount).fill(null); //Create empty array the size of the number of mines.
       let picked = 0;
+      let x = fullField.length, y = fullField[0].length;
       while (picked < randPicks.length) {
-        let xy = (Math.floor(Math.random() * fieldSize)) + "," + (Math.floor(Math.random() * fieldSize));
+        let xy = (Math.floor(Math.random() * x)) + "," + (Math.floor(Math.random() * y));
         if (!randPicks.includes(xy)) { //Make sure the random number hasn't already been picked.
           randPicks[picked] = xy; //Add random number to array of random numbers picked.
           picked++;
@@ -140,7 +136,8 @@ export class Game extends React.Component {
         //Grab the X and Y coordinates.
         let x = coordinates[0];
         let y = coordinates[1];
-        let rowColEnd = fullField.length - 1;
+        let rowEnd = fullField.length - 1;
+        let colEnd = fullField[0].length - 1;
 
         //Do the arithmetic for each of the 8 locations around a mine.
         mineIndicators[0] = [x - 1, y - 1];
@@ -153,16 +150,16 @@ export class Game extends React.Component {
         mineIndicators[7] = [x, y - 1];
 
         //Only check the 8 surrounding squares if X or Y are on the edge.
-        if (x === 0 || x === rowColEnd || y === 0 || y === rowColEnd) {
+        if (x === 0 || x === rowEnd || y === 0 || y === colEnd) {
           //Remove possible locales if they're out of the bounds of the grid.
           mineIndicators.forEach(function (locale, index, array) {
             if (locale[0] < 0) {
               array[index] = null;
-            } else if (locale[0] > rowColEnd) {
+            } else if (locale[0] > rowEnd) {
               array[index] = null;
             } else if (locale[1] < 0) {
               array[index] = null;
-            } else if (locale[1] > rowColEnd) {
+            } else if (locale[1] > colEnd) {
               array[index] = null;
             }
           });
